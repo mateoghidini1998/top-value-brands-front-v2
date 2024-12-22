5fa395751ad6da03e870462b1f5f88cb1d0eedc6
ramiro <ramiro.sarasola@gmail.com>
2024-12-22 13:21:00 -0300

    create actions component for the inventory table

--- src/app/(protected)/inventory/columns.tsx
+++ src/app/(protected)/inventory/columns.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { Product } from "@/app/(protected)/inventory/interfaces/product.interface";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
 
 export const columns: ColumnDef<Product>[] = [
   {
@@ -55,8 +65,32 @@ export const columns: ColumnDef<Product>[] = [
     cell: ({ row }) => <span>{row.original.is_active ? "Yes " : "No"}</span>,
   },
   {
-    accessorKey: "actions",
     id: "actions",
-    header: () => <div className="text-right">Actions</div>,
+    header: "Actions",
+    cell: ({ row }) => {
+      console.log(row.original);
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText("hola")}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
