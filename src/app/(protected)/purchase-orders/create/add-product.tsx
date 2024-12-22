752ee3029df586adb397f37dba70dcad6c5cf1b5
ramiro <ramiro.sarasola@gmail.com>
2024-12-22 18:38:49 -0300

    validate the product is not already added

--- src/app/(protected)/purchase-orders/create/add-product.tsx
+++ src/app/(protected)/purchase-orders/create/add-product.tsx
@@ -3,6 +3,7 @@ import { Dispatch, SetStateAction } from "react";
 import { ProductInOrder } from "./interface/product-added.interface";
 import { TrackedProduct } from "../../inventory/tracked-products/interfaces/tracked-product.interface";
 import { formatTrackedProduct } from "./utils/format-tracked-product";
+import { toast } from "sonner";
 
 interface ActionsCellRow {
   trackedProduct: TrackedProduct;
@@ -14,7 +15,14 @@ const AddProduct = ({ trackedProduct, setData }: ActionsCellRow) => {
 
   const handleAddProduct = () => {
     setData((prev: ProductInOrder[]) => {
-      return [...prev, formattedProduct];
+      // si ya existe un producto con el mismo id, no lo agrega
+      if (prev.some((product) => product.id === formattedProduct.id)) {
+        toast.error("Product already added");
+        return prev;
+      } else {
+        toast.success("Product added successfully");
+        return [...prev, formattedProduct];
+      }
     });
   };
 
