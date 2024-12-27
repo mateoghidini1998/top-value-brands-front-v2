851c21f7028801a63b33171bd33c79601ec38652
ramiro <ramiro.sarasola@gmail.com>
2024-12-27 11:40:54 -0300

    refactor useOrders hook to utilize useQueryClient and extend stale time to 15 minutes

--- src/app/(protected)/purchase-orders/hooks/useOrders.tsx
+++ src/app/(protected)/purchase-orders/hooks/useOrders.tsx
@@ -1,20 +1,21 @@
-import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { createOrder } from "../actions/create-order.action";
 import { getOrders } from "../actions/get-orders.action";
 
 export const useOrders = () => {
-  const queryClient = new QueryClient();
+  // Obtén la instancia de QueryClient proporcionada por el contexto de React Query
+  const queryClient = useQueryClient();
 
   const ordersQuery = useQuery({
     queryKey: ["orders"],
     queryFn: () => getOrders(),
-    staleTime: 1000 * 60 * 5, // -> 5m
+    staleTime: 1000 * 60 * 15, // -> 15m
   });
 
   const createOrderMutation = useMutation({
     mutationFn: createOrder,
     onSuccess: () => {
-      // Invalidate the orders query to refetch the updated list
+      // Invalidate the orders query para obtener la lista actualizada
       queryClient.invalidateQueries({ queryKey: ["orders"] });
     },
   });
