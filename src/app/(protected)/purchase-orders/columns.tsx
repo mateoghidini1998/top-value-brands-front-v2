eedf84c6e3a9b17b4c56db1b5100f87f3c85f91b
ramiro <ramiro.sarasola@gmail.com>
2025-01-20 12:57:29 -0300

    feat: rename avg_roi to average_roi for clarity and update display logic in purchase orders

--- src/app/(protected)/purchase-orders/columns.tsx
+++ src/app/(protected)/purchase-orders/columns.tsx
@@ -62,10 +62,10 @@ export const columns: ColumnDef<Order>[] = [
     },
   },
   {
-    accessorKey: "avg_roi",
+    accessorKey: "average_roi",
     header: "avg roi",
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("avg_roi")).toFixed(2);
+      const amount = parseFloat(row.getValue("average_roi")).toFixed(2);
 
       const getBadgeVariant = (amount: number) => {
         if (amount >= 2) {
@@ -84,7 +84,7 @@ export const columns: ColumnDef<Order>[] = [
           variant={getBadgeVariant(parseFloat(amount))}
           className={`cursor-pointer`}
         >
-          {amount}
+          {!isNaN(parseFloat(amount)) ? amount + "%" : "N/A"}
         </Badge>
       );
     },
