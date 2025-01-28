8b421b942dc3329d67657916b1c42d86f325301e
mateoghidini1998 <mateghidini123@gmail.com>
2025-01-27 22:30:36 -0300

    Hide inventory from regular users

--- src/lib/filter-routes.ts
+++ src/lib/filter-routes.ts
@@ -10,13 +10,13 @@ type RouteItem = {
 export type Route = {
   title: string;
   url: string;
-  icon?: LucideIcon; // Puedes especificar el tipo correcto del icono si lo conoces
+  icon?: LucideIcon; 
   items?: RouteItem[];
 };
 
 export const rolePermissions: Record<Role, string[]> = {
-  admin: ["*"], // Acceso a todas las rutas
-  warehouse: ["/inventory", "/warehouse"],
+  admin: ["*"], 
+  warehouse: ["/warehouse"],
   user: ["/inventory"],
 };
 
@@ -24,7 +24,7 @@ export const filterRoutesByRole = (role: Role, routes: Route[]): Route[] => {
   const allowedRoutes = rolePermissions[role] || [];
 
   if (allowedRoutes.includes("*")) {
-    return routes; // Si el rol tiene acceso total, devuelve todas las rutas
+    return routes; 
   }
 
   return routes.filter(
