421382ef4228fe9d4484ecf1ac9d6e985a1b94ec
ramiro <ramiro.sarasola@gmail.com>
2025-02-11 16:38:57 -0300

    Refactor tracked products page: replace useTrackedProducts with useGetTrackedProducts and update loading/error handling

--- src/app/(protected)/inventory/tracked-products/page.tsx
+++ src/app/(protected)/inventory/tracked-products/page.tsx
@@ -3,23 +3,27 @@
 import { DataTable } from "@/components/custom/data-table";
 import LoadingSpinner from "@/components/custom/loading-spinner";
 import { columns } from "./columns";
-import { useTrackedProducts } from "./hooks";
+import { useGetTrackedProducts } from "./hooks";
 
 export default function Page() {
-  const { trackedProductsQuery } = useTrackedProducts();
+  const { trackedProductsResponse, trackedProductsIsLoading } =
+    useGetTrackedProducts({
+      page: 1,
+      limit: 50,
+    });
 
-  if (trackedProductsQuery.isLoading) {
+  if (trackedProductsIsLoading) {
     return <LoadingSpinner />;
   }
 
-  if (!trackedProductsQuery.data) {
+  if (!trackedProductsResponse) {
     return <div>Error</div>;
   }
 
   return (
     <div>
       <DataTable
-        data={trackedProductsQuery.data.data}
+        data={trackedProductsResponse.data}
         columns={columns}
         dataLength={50}
       />
