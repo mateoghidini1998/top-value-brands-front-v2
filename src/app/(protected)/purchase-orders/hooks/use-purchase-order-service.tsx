28743224d9f9e3f547f533fe34ecb1389e77cafb
ramiro <ramiro.sarasola@gmail.com>
2025-02-11 13:25:30 -0300

    Enable purchase order summary fetching only when orderId is provided

--- src/app/(protected)/purchase-orders/hooks/use-purchase-order-service.tsx
+++ src/app/(protected)/purchase-orders/hooks/use-purchase-order-service.tsx
@@ -212,6 +212,7 @@ export const useGetPurchaseOrderSummary = (orderId: string) => {
     queryFn: () => purchaseOrderService.getPurchaseOrderSummary({ orderId }),
     gcTime: 1000 * 60 * 10, // 10 minutes
     staleTime: 1000 * 60 * 5, // 5 minutes
+    enabled: !!orderId,
   });
 
   return {
