185f6bdbf8f78400354fe9d83dd2644c116de60e
ramiro <ramiro.sarasola@gmail.com>
2025-03-05 12:36:38 -0300

    Remove unused product update logic and transformation functions from PurchaseOrderProvider

--- src/contexts/orders.context.tsx
+++ src/contexts/orders.context.tsx
@@ -34,27 +34,6 @@ export function PurchaseOrderProvider({
     PurchaseOrderProductsUpdates[]
   >([]);
 
-  // const updateProduct = (
-  //   id: number,
-  //   updates: Partial<PurchaseOrderSummaryProducts>
-  // ) => {
-  //   setProducts((currentProducts) => {
-  //     const updatedProducts = currentProducts.map((product) => {
-  //       if (product.id === id) {
-  //         const updatedProduct = { ...product, ...updates };
-  //         // Asegurarse de que total_amount se actualiza correctamente
-  //         updatedProduct.total_amount =
-  //           parseFloat(updatedProduct.product_cost) *
-  //           updatedProduct.quantity_purchased;
-  //         return updatedProduct;
-  //       }
-  //       return product;
-  //     });
-  //     setUpdatedPOProducts(transformProductsUpdates(updatedProducts));
-  //     return updatedProducts;
-  //   });
-  // };
-
   const updateProduct = (
     id: number,
     updates: Partial<PurchaseOrderSummaryProducts>
@@ -125,21 +104,6 @@ export function PurchaseOrderProvider({
     setUpdatedPOProducts((prevUpdated) =>
       prevUpdated.filter((p) => p.purchaseOrderProductId !== id)
     );
-  };
-
-  // transform the products into an array of product updates
-  const transformProductsUpdates = (
-    productsToUpdate: PurchaseOrderSummaryProducts[]
-  ) => {
-    return productsToUpdate.map((product): PurchaseOrderProductsUpdates => {
-      return {
-        product_cost: product.product_cost,
-        profit: product.profit?.toString(),
-        purchaseOrderProductId: product.purchase_order_product_id,
-        quantityPurchased: product.quantity_purchased,
-        unit_price: product.product_cost,
-      };
-    });
   };
 
   return (
